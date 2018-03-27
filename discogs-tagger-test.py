2d9e180eea99550272169f1615ba65a315c83f10
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-03-27 12:53:18 +0200

    Moved test cases to the test directory

--- discogs-tagger-test.py
+++ discogs-tagger-test.py
@@ -1,25 +1,6 @@
 import unittest
 
-from discogstagger.lyrics import LyricsSearcher
-
-class ValidSearchTest(unittest.TestCase):
-    def test(self):
-        searcher = LyricsSearcher("radiohead")
-        searcher.load()
-        lyrics = searcher.search_lyrics("treefingers")
-        self.assertEqual(lyrics, " Instrumental\n")
-
-class InvalidArtistTest(unittest.TestCase):
-    def test(self):
-        searcher = LyricsSearcher("<Invalid Artist Name>")
-        self.assertEqual(searcher.load(), False)
-
-class InvalidTrackTest(unittest.TestCase):
-    def test(self):
-        searcher = LyricsSearcher("radiohead")
-        searcher.load()
-        lyrics = searcher.search_lyrics("<Invalid Track Title>")
-        self.assertEqual(lyrics, "")
+from test.lyrics_test import *
 
 if __name__ == "__main__":
     # Lyrics searcher tests
---
+++ test/lyrics_test.py
@@ -0,0 +1,22 @@
+import unittest
+
+from discogstagger.lyrics import LyricsSearcher
+
+class ValidSearchTest(unittest.TestCase):
+    def test(self):
+        searcher = LyricsSearcher("radiohead")
+        searcher.load()
+        lyrics = searcher.search_lyrics("treefingers")
+        self.assertEqual(lyrics, " Instrumental\n")
+
+class InvalidArtistTest(unittest.TestCase):
+    def test(self):
+        searcher = LyricsSearcher("<Invalid Artist Name>")
+        self.assertEqual(searcher.load(), False)
+
+class InvalidTrackTest(unittest.TestCase):
+    def test(self):
+        searcher = LyricsSearcher("radiohead")
+        searcher.load()
+        lyrics = searcher.search_lyrics("<Invalid Track Title>")
+        self.assertEqual(lyrics, "")
