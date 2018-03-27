8713fe6509765cde126d1c269758e723ad5bcd25
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-03-27 12:23:31 +0200

    Added empty lyrics searcher class

---
+++ lyrics.py
@@ -0,0 +1,11 @@
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+
+import fuzzywuzzy
+
+from bs4 import BeautifulSoup
+
+
+class LyricsSearcher:
+    def __init__(self):
+        pass
