e98e5709c24c119e796d96c2604ee8a99e9b2d96
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-03-27 12:25:07 +0200

    Added web crawler class

---
+++ crawler.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+
+
+class WebCrawler:
+    def __init__(self):
+        self.header = {
+            'User-Agent': 'Discogs Tagger User Agent 1.0'
+        }
+
+    def get_response(self, url):
+        return requests.get(url, headers=self.header)
+
+    def get_soup(self, url):
+        return BeautifulSoup(self.get_response(url).text, "html.parser")
