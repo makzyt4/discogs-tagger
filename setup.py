3514cf662096669bb173835f35de342725ea2cbe
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-03-28 20:14:21 +0200

    Modified setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ setup(
     name="DiscogsTagger",
     version="0.1 beta",
     packages=['discogstagger'],
+    entry_points={
+        'console_scripts': [
+            'discogs-tagger = discogstagger.__main__:main'
+        ]
+    },
     license='MIT License',
     long_description=open('README.md').read()
 )
