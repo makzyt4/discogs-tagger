8b9149895a356a26899140c0306061da5eb5a43d
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-03-28 19:41:01 +0200

    Added setup.py and MANIFESTs

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="DiscogsTagger",
+    version="0.1 beta",
+    packages=['discogstagger'],
+    license='MIT License',
+    long_description=open('README.md').read()
+)
