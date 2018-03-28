fa661c1f0c874838af050c06a29bec6033976f97
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-03-28 21:25:33 +0200

    Added requirements to setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,13 @@ setup(
             'discogs-tagger = discogstagger.__main__:main'
         ]
     },
+    install_requires=[
+        'beautifulsoup4==4.6.0',
+        'fuzzywuzzy==0.16.0',
+        'mutagen==1.40.0',
+        'requests==2.17.3',
+        'urllib3==1.22'
+    ]
     description='Discogs album tags downloader',
     author='Maksymilian Zytkiewicz',
     author_email='maksymilian.zytkiewicz@gmail.com',
