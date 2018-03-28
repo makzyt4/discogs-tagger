56ffb0d1d4830aa7b7a402d2e14f2919feb3632f
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-03-28 21:17:01 +0200

    Modified setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from distutils.core import setup
 
 setup(
-    name="DiscogsTagger",
-    version="0.1 beta",
+    name="discogs-tagger",
+    version="0.1.0",
     packages=['discogstagger'],
     entry_points={
         'console_scripts': [
             'discogs-tagger = discogstagger.__main__:main'
         ]
     },
-    license='MIT License',
+    description='Discogs album tags downloader',
+    author='Maksymilian Zytkiewicz',
+    author_email='maksymilian.zytkiewicz@gmail.com',
+    url='https://github.com/makzyt4/discogs-tagger',
+    keywords=['music', 'webcrawling'],
+    license=open('LICENSE').read(),
     long_description=open('README.md').read()
 )
